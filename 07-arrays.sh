473b8c51652bf9ac9b1fbddb50a6e365b320ebf2
nirmala <nirmala.murari08@gmail.com>
2026-05-05 16:07:38 +0530

    added

---
+++ 07-arrays.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+MOVIES=("PUSHPA" "RRR" "DEVARA")
+# INDEX STARTS FROM 0,SIZE IS 3
+
+echo "first movie: ${MOVIES{0}}"
+echo "second movie: ${MOVIES{1}}"
+echo "third movie: ${MOVIES{2}}"
+
+echo "All movies are: ${MOVIES[@]}"
