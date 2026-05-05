7867eda36fb949c1874c7c6024232104466ee2e4
nirmala <nirmala.murari08@gmail.com>
2026-05-05 14:05:02 +0530

    added

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Ramesh # no space before and after equal
+PERSON2=Suresh
+
+
+echo "$PERSON1: hi $PERSON2"
+echo "$PESRSON2: hello $PERSON1"
+echo "$PERSON1:how r u"
+echo "$PERSON2: i am good. how are you?"
